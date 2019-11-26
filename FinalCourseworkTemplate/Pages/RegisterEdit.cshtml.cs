261fd760297fb5b9486ac2cba1d4aaeb28b64178
Leachyboi <nkraevoy@live.com>
2019-11-26 09:46:29 +0000

    RegisterEdit

---
+++ FinalCourseworkTemplate/Pages/RegisterEdit.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalCourseworkTemplate.Pages
+{
+    public class RegisterEditModel : PageModel
+    {
+        [BindProperty]
+        public bool Attend { get; set; }
+
+        [TempData]
+        public string answer { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (Attend == true)
+            {
+                answer = "Yes";
+            }
+            else
+            {
+                answer = "No";
+            }
+            return RedirectToPage("./RegisterEdit");
+        }
+    }
+}
