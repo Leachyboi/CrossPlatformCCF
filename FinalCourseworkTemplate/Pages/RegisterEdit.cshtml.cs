fb512a722119f4b275d7f7ded4ec2902b91f302e
Leachyboi <nkraevoy@live.com>
2019-12-02 10:31:37 +0000

    data collecting

--- FinalCourseworkTemplate/Pages/RegisterEdit.cshtml.cs
+++ FinalCourseworkTemplate/Pages/RegisterEdit.cshtml.cs
@@ -78,93 +78,104 @@ namespace FinalCourseworkTemplate.Pages
 
         public async Task<IActionResult> OnPostAsync()
         {
-            var registers = _context.Registers.Where(r => r.DateOfReg == new DateTime(2019, 11, 26)).ToList();
+            DateTime inputDate = date;
+            var cadets = _context.Cadets.ToList();
+            var registers = _context.Registers.ToList();
+            var cadetRegisters = _context.CadetRegisters.Where(r => r.Register.DateOfReg == inputDate).ToList();
 
-            _context.Registers.Add(new Register {Attended = true, 
-            DateOfReg = new DateTime(),
-            });
-            await _context.SaveChangesAsync();
-            
-            //assigns value to variable depending on input
-            //foreach(var cadet in RegisterViews)
-            //{
-            //    var s = $"{cadet.FullName}, {cadet.Attendance}, {cadet.RegDate}";
-            //    if (cadet.Attendance == "true")
-            //    {
-            //        answer = "Yes";
-            //    }
-            //    else
-            //    {
-            //        answer = "No";
-            //    }
-            //}
-            //string queryString =
-            //"SELECT CadetId, RegisterId FROM dbo.CadetRegisters;";
+            string firstName = cadetRegisters[0].Cadet.KnownAs;
+            string lastName = cadetRegisters[0].Cadet.Surname;
+            string attend = cadetRegisters[0].Register.Attended.ToString();
+            string newDate = cadetRegisters[0].Register.DateOfReg.ToShortDateString();
 
-            //string queryStringAttend =
-            //"SELECT Attended, DateOfReg FROM dbo.Register;";
+            //_context.Registers.Add(new Register {Attended = true, 
+            //DateOfReg = new DateTime(),
+            //});
 
-            //using (SqlConnection connection =
-            //           new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=CourseworkDatabase;Trusted_Connection=True;"))
-            //{
-            //    SqlCommand command =
-            //        new SqlCommand(queryString, connection);
-            //    connection.Open();
+            //await _context.SaveChangesAsync();
 
-            //    SqlDataReader reader = command.ExecuteReader();
+            /*assigns value to variable depending on input
+            foreach (var cadet in RegisterViews)
+            {
+                var s = $"{cadet.FullName}, {cadet.Attendance}, {cadet.RegDate}";
+                if (cadet.Attendance == "true")
+                {
+                    answer = "Yes";
+                }
+                else
+                {
+                    answer = "No";
+                }
+            }
+            string queryString =
+            "SELECT CadetId, RegisterId FROM dbo.CadetRegisters;";
 
-            //    // Call Read before accessing data.
-            //    while (reader.Read())
-            //    {
-            //        string regId = Convert.ToString(reader[0]);
+            string queryStringAttend =
+            "SELECT Attended, DateOfReg FROM dbo.Register;";
 
-            //        SqlCommand commandAttended =
-            //        new SqlCommand(queryStringAttend, connection);
-            //        connection.Open();
-            //        SqlDataReader readerAttended = commandAttended.ExecuteReader();
+            using (SqlConnection connection =
+                       new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=CourseworkDatabase;Trusted_Connection=True;"))
+            {
+                SqlCommand command =
+                    new SqlCommand(queryString, connection);
+                connection.Open();
 
-            //        returnedString = testFunc(regId, Convert.ToString(readerAttended[0]), Convert.ToDateTime(readerAttended[1]).Date);
-            //    }
+                SqlDataReader reader = command.ExecuteReader();
 
-            //    // Call Close when done reading.
-            //    reader.Close();
-            //    connection.Close();
-            //}
-            //using (v
-            //    var register = DbConnection.Database.SqlQuery("SELECT * FROM Registers").ToList;
-            
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    string regId = Convert.ToString(reader[0]);
 
-            //useDate = date.ToShortDateString();
+                    SqlCommand commandAttended =
+                    new SqlCommand(queryStringAttend, connection);
+                    connection.Open();
+                    SqlDataReader readerAttended = commandAttended.ExecuteReader();
+
+                    returnedString = testFunc(regId, Convert.ToString(readerAttended[0]), Convert.ToDateTime(readerAttended[1]).Date);
+                }
+
+                // Call Close when done reading.
+                reader.Close();
+                connection.Close();
+            }
+            using (v
+                var register = DbConnection.Database.SqlQuery("SELECT * FROM Registers").ToList;
+
+            */
+            returnedString = $"{firstName} {lastName}'s Attendance on {newDate} is {attend}.";
+            useDate = date.ToShortDateString();
             return RedirectToPage("./RegisterEdit");
         }
         
-        public string testFunc(string cadId, string attendOrNot, DateTime dateReg)
+        public string testFunc()
         {
-            //string newQuery = $"SELECT CadetId, Surname, KnownAs FROM dbo.Cadets WHERE CadetId = {cadId};";
+            /*string newQuery = $"SELECT CadetId, Surname, KnownAs FROM dbo.Cadets WHERE CadetId = {cadId};";
 
-            //using (SqlConnection connection
-            //    = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=CourseworkDatabase;Trusted_Connection=True;"))
-            //{
-            //    SqlCommand command =
-            //        new SqlCommand(newQuery, connection);
-            //    connection.Open();
+            using (SqlConnection connection
+                = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=CourseworkDatabase;Trusted_Connection=True;"))
+            {
+                SqlCommand command =
+                    new SqlCommand(newQuery, connection);
+                connection.Open();
 
-            //    SqlDataReader reader = command.ExecuteReader();
+                SqlDataReader reader = command.ExecuteReader();
 
-            //    // Call Read before accessing data.
-            //    while (reader.Read())
-            //    {
-            //        Surname = Convert.ToString(reader[1]);
-            //        KnownAs = Convert.ToString(reader[2]);
-            //    }
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    Surname = Convert.ToString(reader[1]);
+                    KnownAs = Convert.ToString(reader[2]);
+                }
 
-            //    // Call Close when done reading.
-            //    reader.Close();
-            //    connection.Close();
-            //}
+                // Call Close when done reading.
+                reader.Close();
+                connection.Close();
+            }
 
             string sentence = $"{KnownAs} {Surname} Attendance on {dateReg} is {attendOrNot}.";
-            return sentence;
+            return sentence;*/
+            return "";
         }
     }
 }
