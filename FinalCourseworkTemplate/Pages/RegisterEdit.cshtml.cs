d0896db5c517c871a284b76d3b3b88e831fd1fd3
Leachyboi <nkraevoy@live.com>
2020-01-21 11:39:24 +0000

    multiplebuttons

--- FinalCourseworkTemplate/Pages/RegisterEdit.cshtml.cs
+++ FinalCourseworkTemplate/Pages/RegisterEdit.cshtml.cs
@@ -28,11 +28,6 @@ namespace FinalCourseworkTemplate.Pages
         public string regGroup { get; set; }
         [BindProperty]
         public int regGroupVal { get; set; }
-        public string memStr { get; set; }
-        public int memNum { get; set; }
-
-        [BindProperty]
-        public DateTime date { get; set; } = DateTime.Today.Date;
 
         [BindProperty]
         public string name { get; set; }
@@ -44,9 +39,6 @@ namespace FinalCourseworkTemplate.Pages
         [TempData]
         public string returnedString { get; set; }
 
-        [TempData]
-        public string useDate { get; set; }
-
         DateTime day;
         int counttest;
 
@@ -56,35 +48,12 @@ namespace FinalCourseworkTemplate.Pages
             RegisterViews = new List<RegisterView>();
             Cadets = _context.Cadets.Include(c => c.Registers).OrderBy(s => s.Surname).ToList();
 
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                day = DateTime.Today;
-            }
-            else
-            {
-                day = DateTime.Today.AddDays(-1);
-                while (day.DayOfWeek != DayOfWeek.Tuesday)
-                {
-                    day = day.AddDays(-1);
-                }
-            }
+            day = daySelector(DateTime.Today);
 
             Registers = _context.Registers.Where(c => c.DateOfReg == day).ToList();
             
             if(Registers.Count == 0)
             {
-                if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-                {
-                    day = DateTime.Today;
-                }
-                else
-                {
-                    day = DateTime.Today.AddDays(-1);
-                    while(day.DayOfWeek != DayOfWeek.Tuesday) 
-                    {
-                        day = day.AddDays(-1);
-                    }
-                }
                 _context.Registers.Add(new Register { Attended = true, DateOfReg = day });
                 _context.Registers.Add(new Register { Attended = false, DateOfReg = day });
                 _context.SaveChanges();
@@ -144,104 +113,117 @@ namespace FinalCourseworkTemplate.Pages
             }
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public DateTime daySelector (DateTime currentDay)
         {
-            //DateTime inputDate = date;
-            //bool attendance = Attend;
-            RegisterViews = new List<RegisterView>();
+            if (currentDay.DayOfWeek == DayOfWeek.Tuesday)
+            {
+            }
+            else
+            {
+                currentDay = currentDay.AddDays(-1);
+                while (currentDay.DayOfWeek != DayOfWeek.Tuesday)
+                {
+                    currentDay = currentDay.AddDays(-1);
+                }
+            }
+            return currentDay;
+        }
+
+        public async Task<IActionResult> OnPostFilterAsync()
+        {
             Cadets = _context.Cadets.Include(c => c.Registers).OrderBy(s => s.Surname).ToList();
             Registers = _context.Registers.ToList();
-            //var cadetRegisters = _context.CadetRegisters.Where(r => r.RegisterId == r.Register.RegisterId && r.Register.DateOfReg == inputDate && r.Cadet.Surname == inputName).ToList();
-            if (memStr != regGroup && memNum != regGroupVal)
+
+            RegisterViews = new List<RegisterView>();
+            if (regGroup == "years")
             {
-                if (regGroup == "years")
+                if (regGroupVal >= 9 && regGroupVal <= 13)
                 {
-                    if (regGroupVal >= 9 && regGroupVal <= 13)
-                    {
-                        Cadets = Cadets.Where(s => s.Year == regGroupVal).ToList();
-                        memStr = "years";
-                        memNum = regGroupVal;
-                    }
-                    else
-                    {
-                    }
-                }
-                else if (regGroup == "platoons")
-                {
-                    if (regGroupVal >= 1 && regGroupVal <= 3)
-                    {
-                        Cadets = Cadets.Where(s => s.Platoon == regGroupVal).ToList();
-                        memStr = "platoons";
-                        memNum = regGroupVal;
-                    }
-                    else
-                    {
-                    }
+                    Cadets = Cadets.Where(s => s.Year == regGroupVal).ToList();
                 }
                 else
                 {
-                    memStr = "All";
-                    memNum = 1;
                 }
-
-                foreach (var cadet in Cadets)
+            }
+            else if (regGroup == "platoons")
+            {
+                if (regGroupVal >= 1 && regGroupVal <= 3)
                 {
-                    if (0 == cadet.Registers.Count)
+                    Cadets = Cadets.Where(s => s.Platoon == regGroupVal).ToList();
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+            }
+
+            day = daySelector(DateTime.Today);
+
+            foreach (var cadet in Cadets)
+            {
+                if (0 == cadet.Registers.Count)
+                {
+                    RegisterViews.Add(
+                        new RegisterView
+                        {
+                            FullName = cadet.Surname + ", " + cadet.KnownAs,
+                            Attendance = false.ToString(),
+                            RegDate = day.Date,
+                        }
+                    );
+                }
+                else
+                {
                     {
                         RegisterViews.Add(
                             new RegisterView
                             {
                                 FullName = cadet.Surname + ", " + cadet.KnownAs,
-                                Attendance = false.ToString(),
+                                Attendance = false.ToString(),// ? "Yes" : "No",
                                 RegDate = day.Date,
                             }
                         );
                     }
-                    else
-                    {
-                        {
-                            RegisterViews.Add(
-                                new RegisterView
-                                {
-                                    FullName = cadet.Surname + ", " + cadet.KnownAs,
-                                    Attendance = false.ToString(),// ? "Yes" : "No",
-                                    RegDate = DateTime.Today,
-                                }
-                            );
-                        }
-                    }
                 }
-                return Page();
             }
+            return Page();           
+        }
 
-            else
+        public async Task<IActionResult> OnPostSubmitAsync()
+        {
+            //DateTime inputDate = date;
+            //bool attendance = Attend;
+            Cadets = _context.Cadets.Include(c => c.Registers).OrderBy(s => s.Surname).ToList();
+            Registers = _context.Registers.ToList();
+            //var cadetRegisters = _context.CadetRegisters.Where(r => r.RegisterId == r.Register.RegisterId && r.Register.DateOfReg == inputDate && r.Cadet.Surname == inputName).ToList();
+
+            foreach (var regEntry in RegisterViews)
             {
-                foreach (var regEntry in RegisterViews)
+                string inpName = regEntry.FullName;
+
+                string[] splitName = inpName.Split(',');
+                string realName = $"{splitName[1]} {splitName[0]}".Trim();
+
+                var cadetQuer = _context.Cadets.Where(c => c.Surname == splitName[0].Trim() && c.KnownAs == splitName[1].Trim()).ToList();
+                var attendance = 0 == string.Compare(regEntry.Attendance, "true", StringComparison.CurrentCultureIgnoreCase);
+                var regQuer = _context.Registers
+                    .Where(r => r.DateOfReg == regEntry.RegDate && r.Attended == attendance).ToList();
+                if (regQuer.Count > 0 && cadetQuer.Count > 0)
                 {
-                    string inpName = regEntry.FullName;
-
-                    string[] splitName = inpName.Split(',');
-                    string realName = $"{splitName[1]} {splitName[0]}".Trim();
-
-                    var cadetQuer = _context.Cadets.Where(c => c.Surname == splitName[0].Trim() && c.KnownAs == splitName[1].Trim()).ToList();
-                    var attendance = 0 == string.Compare(regEntry.Attendance, "true", StringComparison.CurrentCultureIgnoreCase);
-                    var regQuer = _context.Registers
-                        .Where(r => r.DateOfReg == regEntry.RegDate && r.Attended == attendance).ToList();
-                    if (regQuer.Count > 0 && cadetQuer.Count > 0)
-                    {
-                        int cadID = cadetQuer[0].CadetId;
-                        int regID = regQuer[0].RegisterId;
-                        _context.CadetRegisters.Add(new CadetRegister { CadetId = cadID, RegisterId = regID });
-                        counttest++;
-                        returnedString = counttest + " Entry Created";
-                    }
-                    if (cadetQuer.Count == 0)
-                    {
-                        returnedString = "Cadet doesn't exist";
-                    }
+                    int cadID = cadetQuer[0].CadetId;
+                    int regID = regQuer[0].RegisterId;
+                    _context.CadetRegisters.Add(new CadetRegister { CadetId = cadID, RegisterId = regID });
+                    counttest++;
+                    returnedString = counttest + " Entry Created";
                 }
-                return Page();
+                if (cadetQuer.Count == 0)
+                {
+                    returnedString = "Cadet doesn't exist";
+                }
             }
+            return Page();
             /*if (cadetRegisters.Count > 0)
             {
                 string firstName = cadetRegisters[0].Cadet.KnownAs;
@@ -339,42 +321,6 @@ namespace FinalCourseworkTemplate.Pages
                 var register = DbConnection.Database.SqlQuery("SELECT * FROM Registers").ToList;
 
             */
-        }
-        
-
-
-        public string stringCreate(string fname, string sname, string shortdate, string attended)
-        {
-            
-            string newString = $"{fname} {sname}'s Attendance on {shortdate} is {attended}.";
-            return newString;
-            
-            /*string newQuery = $"SELECT CadetId, Surname, KnownAs FROM dbo.Cadets WHERE CadetId = {cadId};";
-
-            using (SqlConnection connection
-                = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=CourseworkDatabase;Trusted_Connection=True;"))
-            {
-                SqlCommand command =
-                    new SqlCommand(newQuery, connection);
-                connection.Open();
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                // Call Read before accessing data.
-                while (reader.Read())
-                {
-                    Surname = Convert.ToString(reader[1]);
-                    KnownAs = Convert.ToString(reader[2]);
-                }
-
-                // Call Close when done reading.
-                reader.Close();
-                connection.Close();
-            }
-
-            string sentence = $"{KnownAs} {Surname} Attendance on {dateReg} is {attendOrNot}.";
-            return sentence;*/
-            
         }
     }
 }
